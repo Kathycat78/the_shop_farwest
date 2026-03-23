cf806879497a5cc6731ba7e91b91bf63c31d3393
Kathycat78 <Kathy@78>
2026-03-23 16:34:45 +0100

    the_shop_farwest

---
+++ tests/ForumControllerTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ForumControllerTest extends WebTestCase
+{
+    public function testSomething(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', 'Les Petits Choux🐉');
+    }
+     public function testPageNotFound(): void
+    {
+        // On crée un client HTTP simulé (comme un navigateur)
+        $client = static::createClient();
+
+        // On teste une URL qui n'existe pas
+        $client->request('GET', '/route-qui-existe-pas');
+
+        // ✅ Le site doit répondre 404
+        $this->assertResponseStatusCodeSame(404);
+    }
+
+    public function testRedirectWhenNotLogged(): void
+    {
+        // On crée un client HTTP simulé (comme un navigateur)
+        $client = static::createClient();
+
+        // On tente d'accéder à une page protégée
+        $client->request('GET', '/comment');
+
+        // ✅ On doit être redirigé vers la page login
+        $this->assertResponseRedirects('/login');
+    }
+}
