dea598f48c9186bfb3589699d0a54eae941fa179
Kathycat78 <Kathy@78>
2026-04-10 09:43:38 +0200

    the_shop_farwest

--- src/Views/home.view.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once(__DIR__ . "/partials/head.view.php");
-?>
-<h1>Bienvenue 👽👽👻</h1>
-<?php
-require_once(__DIR__ . "/partials/footer.view.php");
--- src/Views/partials/footer.view.php
+++
@@ -1,6 +0,0 @@
-<footer>
-    <p> © <?= date('Y') ?> - The_shop_farwest</p>
-</footer>
-</body>
-
-</html>
